33fff61af65be1737884f15badfc95b67e5fec38
Mykhailo Semitkin <michael.semitkin@gmail.com>
2022-06-17 00:10:23 +0300

    implemented Vigenere cipher decryption

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,14 @@
+import algoriths.cryptographic.decrypt
 import algoriths.cryptographic.encrypt
 
 fun main() {
-    println(encrypt("hello world", "encryptionkey"))
+    val textToEncrypt = "the quick brown fox jumps over 13 lazy dogs."
+    val encryptionKey = "cryptii"
+
+    val encrypted = encrypt(textToEncrypt, encryptionKey)
+    println(encrypted)
+
+    val decrypted = decrypt(encrypted, encryptionKey)
+    println(decrypted)
+
 }
--- src/main/kotlin/algoriths/cryptographic/VigenereCipher.kt
+++ src/main/kotlin/algoriths/cryptographic/VigenereCipher.kt
@@ -1,18 +1,50 @@
 package algoriths.cryptographic
 
-const val ALPHABET = "abcdefghijklmnopqrstuvwxyz"
+import java.util.Collections.rotate
 
-fun encrypt(inputText: String, key: String): String {
+private const val ALPHABET = "abcdefghijklmnopqrstuvwxyz"
+private val ENCRYPTION_MATRIX: List<List<Char>> = ALPHABET
+    .mapIndexed { index, _ -> ALPHABET.toList().rotate(-1 * index) }
+
+fun encrypt(textToEncrypt: String, encryptionKey: String): String {
     var index = 0;
-    val words = inputText.split(" ")
+    val words = textToEncrypt.split(" ")
     val encryptedWords = words.map { word ->
         word.map { character ->
-            character.encrypt(key[index++ % key.length])
+            character.encrypt(encryptionKey[index++ % encryptionKey.length])
         }.joinToString(separator = "")
     }
     return encryptedWords.joinToString(separator = " ")
 }
 
-fun Char.alphabetCode() = ALPHABET.indexOf(this)
+fun decrypt(encryptedText: String, encryptionKey: String): String {
+    var index = 0;
+    val words = encryptedText.split(" ")
+    val decryptedWords = words.map { word ->
+        word.map { character ->
+            character.decrypt(encryptionKey[index++ % encryptionKey.length])
+        }.joinToString(separator = "")
+    }
+    return decryptedWords.joinToString(separator = " ")
+}
 
-fun Char.encrypt(key: Char) = ALPHABET[(alphabetCode() + key.alphabetCode()) % 26]
+private fun Char.encrypt(key: Char) =
+    with(alphabetCode()) {
+        if (this != -1) ENCRYPTION_MATRIX[this][key.alphabetCode()] else this@encrypt
+    }
+
+private fun Char.decrypt(key: Char) =
+    with(key.alphabetCode()) {
+        val line = ENCRYPTION_MATRIX[this]
+        val indexOfRealCharacter = line.indexOf(this@decrypt)
+        if (indexOfRealCharacter != -1) ALPHABET[indexOfRealCharacter] else this@decrypt
+    }
+
+private fun Char.alphabetCode() = ALPHABET.indexOf(this)
+
+
+private fun <T> List<T>.rotate(distance: Int): List<T> {
+    val list = ArrayList(this)
+    rotate(list, distance)
+    return list
+}
