8c7d9f9143b45378335a83433f78cc71099f7f66
Mykhailo Semitkin <michael.semitkin@gmail.com>
2022-06-17 22:27:13 +0300

    correctly process not supported symbols

--- src/main/kotlin/algoriths/cryptographic/VigenereCipher.kt
+++ src/main/kotlin/algoriths/cryptographic/VigenereCipher.kt
@@ -7,22 +7,34 @@ private val ENCRYPTION_MATRIX: List<List<Char>> = ALPHABET
     .mapIndexed { index, _ -> ALPHABET.toList().rotate(-1 * index) }
 
 fun encrypt(textToEncrypt: String, encryptionKey: String): String {
-    var index = 0;
+    var index = 0
     val words = textToEncrypt.split(" ")
     val encryptedWords = words.map { word ->
-        word.map { character ->
-            character.encrypt(encryptionKey[index++ % encryptionKey.length])
+        word.map { plainCharacter ->
+            try {
+                val encrypted = plainCharacter.encrypt(encryptionKey[index % encryptionKey.length])
+                index++
+                encrypted
+            } catch (e: IllegalArgumentException) {
+                plainCharacter
+            }
         }.joinToString(separator = "")
     }
     return encryptedWords.joinToString(separator = " ")
 }
 
 fun decrypt(encryptedText: String, encryptionKey: String): String {
-    var index = 0;
+    var index = 0
     val words = encryptedText.split(" ")
     val decryptedWords = words.map { word ->
         word.map { character ->
-            character.decrypt(encryptionKey[index++ % encryptionKey.length])
+            try {
+                val decryptedCharacter = character.decrypt(encryptionKey[index % encryptionKey.length])
+                index++
+                decryptedCharacter
+            } catch (e: IllegalArgumentException) {
+                character
+            }
         }.joinToString(separator = "")
     }
     return decryptedWords.joinToString(separator = " ")
@@ -30,14 +42,14 @@ fun decrypt(encryptedText: String, encryptionKey: String): String {
 
 private fun Char.encrypt(key: Char) =
     with(alphabetCode()) {
-        if (this != -1) ENCRYPTION_MATRIX[this][key.alphabetCode()] else this@encrypt
+        if (this != -1) ENCRYPTION_MATRIX[this][key.alphabetCode()] else throw IllegalArgumentException()
     }
 
 private fun Char.decrypt(key: Char) =
     with(key.alphabetCode()) {
         val line = ENCRYPTION_MATRIX[this]
         val indexOfRealCharacter = line.indexOf(this@decrypt)
-        if (indexOfRealCharacter != -1) ALPHABET[indexOfRealCharacter] else this@decrypt
+        if (indexOfRealCharacter != -1) ALPHABET[indexOfRealCharacter] else throw IllegalArgumentException()
     }
 
 private fun Char.alphabetCode() = ALPHABET.indexOf(this)
@@ -48,5 +60,3 @@ private fun <T> List<T>.rotate(distance: Int): List<T> {
     rotate(list, distance)
     return list
 }
-
-//TODO indexes should not increment when not supported symbol found
